a3e401317fb546db8a999f9439c9397b56ec606d
William Wold <wm@wmww.sh>
2019-07-03 09:43:03 +0000

    Add env variables to force Qt and GTK to use Wayland

--- glue/setup-env.sh
+++ glue/setup-env.sh
@@ -2,3 +2,9 @@
 
 # For a classic snap this needs to be something sane
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
+
+# Force Wayland for Qt apps
+export QT_QPA_PLATFORM=wayland
+
+# GTK apps will generally default to Wayland, but some (like Firefox) need the extra hint
+export GDK_BACKEND=wayland
