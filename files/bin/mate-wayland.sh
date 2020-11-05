282aad6627f47c38c15bdf93dcf1352273bd098e
William Wold <wm@wmww.sh>
2020-11-04 22:16:21 -0800

    Fully enable XWayland

--- files/bin/mate-wayland.sh
+++ files/bin/mate-wayland.sh
@@ -4,10 +4,24 @@ set -euo pipefail
 export WAYLAND_DISPLAY=wayland-mate # This will be the Wayland display Mirco creates
 export XDG_RUNTIME_DIR=/run/user/$(id -u) # Since this is a classic snap, this should be set to the normal value
 export QT_QPA_PLATFORM=wayland # Force Wayland for Qt apps
-export GDK_BACKEND=wayland # GTK apps will generally default to Wayland, but some (like Firefox) need the extra hint
+export GDK_BACKEND=wayland,x11 # Ask GTK apps to prefer Wayland, but allow them to fall back to X11 if they must
+export MOZ_ENABLE_WAYLAND=1 # Make Firefox use Wayland
 
-${SNAP}/bin/mirco.sh $@ &
+# Once Mir starts up, it will drop the X11 display number into this file
+XWAYLAND_DISPLAY_FILE=$(mktemp mir-x11-display.XXXXXX --tmpdir)
+
+"$SNAP"/bin/mirco.sh $@ --x11-displayfd 5 5>"$XWAYLAND_DISPLAY_FILE" &
 SERVER_PID=$!
+
+echo "Waiting for DISPLAY to appear in $XWAYLAND_DISPLAY_FILE"
+# The inotify solution doesn't seem to work
+# "$SNAP"/usr/bin/inotifywait --event close_write "$XWAYLAND_DISPLAY_FILE"
+while test -z $(cat "$XWAYLAND_DISPLAY_FILE"); do
+  sleep 0.05
+done
+export DISPLAY=":$(cat "$XWAYLAND_DISPLAY_FILE")"
+rm "$XWAYLAND_DISPLAY_FILE"
+echo "DISPLAY=$DISPLAY, $XWAYLAND_DISPLAY_FILE deleted"
 
 # Wait for the socket to appear
 SOCKET_PATH="$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY"
@@ -30,6 +44,7 @@ done
 
 echo "WAYLAND_DISPLAY: ${WAYLAND_DISPLAY:-[nil]}"
 echo "XDG_RUNTIME_DIR: ${XDG_RUNTIME_DIR:-[nil]}"
+echo "DISPLAY: ${DISPLAY:-[nil]}"
 
 COMPONENT_PIDS=""
 
