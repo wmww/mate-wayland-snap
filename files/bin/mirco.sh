2b20ab79e678ca6a7f22455d86230b2c30c7ee79
William Wold <wm@wmww.sh>
2021-03-24 12:50:48 -0700

    Pull in some changes from egmde

--- files/bin/mirco.sh
+++ files/bin/mirco.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 export MIR_CLIENT_PLATFORM_PATH=${SNAP}/usr/lib/${SNAPCRAFT_ARCH_TRIPLET}/mir/client-platform
 export MIR_SERVER_PLATFORM_PATH=${SNAP}/usr/lib/${SNAPCRAFT_ARCH_TRIPLET}/mir/server-platform
 export __EGL_VENDOR_LIBRARY_DIRS=${SNAP}/etc/glvnd/egl_vendor.d:${SNAP}/usr/share/glvnd/egl_vendor.d
+export LIBGL_DRIVERS_PATH=${SNAP}/usr/lib/${SNAPCRAFT_ARCH_TRIPLET}/dri
 export MIR_SERVER_ENABLE_X11=1
 
 # Run server
