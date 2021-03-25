d6ee604f9476d2c172e7cbe7c992ef31aa068923
William Wold <wm@wmww.sh>
2021-03-24 18:12:37 -0700

    Fix for running on Ubuntu 21.04

--- files/bin/mirco.sh
+++ files/bin/mirco.sh
@@ -6,6 +6,8 @@ export MIR_SERVER_PLATFORM_PATH=${SNAP}/usr/lib/${SNAPCRAFT_ARCH_TRIPLET}/mir/se
 export __EGL_VENDOR_LIBRARY_DIRS=${SNAP}/etc/glvnd/egl_vendor.d:${SNAP}/usr/share/glvnd/egl_vendor.d
 export LIBGL_DRIVERS_PATH=${SNAP}/usr/lib/${SNAPCRAFT_ARCH_TRIPLET}/dri
 export MIR_SERVER_ENABLE_X11=1
+export LD_LIBRARY_PATH=${SNAP}/usr/lib/${SNAPCRAFT_ARCH_TRIPLET}:${SNAP}/lib/${SNAPCRAFT_ARCH_TRIPLET}
+export XDG_RUNTIME_DIR=/run/user/${UID}
 
 # Run server
-exec dbus-run-session -- ${SNAP}/usr/local/bin/mirco $@
+exec ${SNAP}/usr/local/bin/mirco $@
